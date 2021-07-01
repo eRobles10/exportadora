756a93d9039282c4f72a4020139ff01d9f86d62d
Edgar <edgar.robles10@gmail.com>
2021-07-01 04:26:13 -0500

    update company principles in home

--- webexportadores/core/views.py
+++ webexportadores/core/views.py
@@ -6,6 +6,8 @@ from about.models import About
 from django.shortcuts import get_object_or_404
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
+from django.utils.translation import gettext as _
+from django.utils.translation import get_language, activate, gettext
 # Create your views here.
 """
 Inicio
@@ -21,19 +23,19 @@ Servicios
 class HomePageView(TemplateView):
 
     template_name = "core/home.html"
-    news = ""
     certification = get_object_or_404(Certification, order=0)
     process_intro = get_object_or_404(Process, pk=1)
-    """Process.objects.get()"""
-    about = get_object_or_404(About, order=0)
+    about = get_object_or_404(About, pk=1)
+    about_principles = About.objects.exclude(pk=1)
 
     def get(self, request, **kwargs):
 
         return render(request,
-                      self.template_name, {'news': self.news,
-                                           'certification': self.certification,
-                                           'process_intro': self.process_intro,
-                                           'about': self.about})
+                      self.template_name, {
+                          'certification': self.certification,
+                          'process_intro': self.process_intro,
+                          'about': self.about,
+                          'about_principles': self.about_principles})
 
 
 def contact(request):
